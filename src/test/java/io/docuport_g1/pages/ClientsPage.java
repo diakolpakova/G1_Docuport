38b03d6765189d425eb318d105da0902b3dc31ed
ZakharYedigarov <yedigarov17@gmail.com>
2025-03-24 10:03:03 -0700

    Login and logout feature complete

--- src/test/java/io/docuport_g1/pages/ClientsPage.java
+++ src/test/java/io/docuport_g1/pages/ClientsPage.java
@@ -5,7 +5,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -96,7 +99,9 @@ public class ClientsPage {
                     WebElement advisorOption = Driver.getDriver().findElement(By.xpath("//div[contains(text(),'" + fieldValue + "')]"));
                     advisorOption.click();
                 } else {
-                    field.clear();
+                    WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+
+                    wait.until(ExpectedConditions.visibilityOf(field)).clear();
                     field.sendKeys(fieldValue);
                 }
             } else {
