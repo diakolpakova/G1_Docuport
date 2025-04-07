497bc77f48ceaad802f4a171e871400cca1e4003
dianakolpakova <dia.kolpakova@gmail.com>
2025-04-06 20:16:33 -0400

    aaaa

--- src/test/java/io/docuport_g1/runner/CukesRunner.java
+++ src/test/java/io/docuport_g1/runner/CukesRunner.java
@@ -7,8 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {//"pretty",
-                "html:target/html-reports",
-                "json:target/json-reports",
+                "html:target/html-reports/cucumber-report.html",
+                "json:target/json-reports/json-report.json",
                 "rerun:target/rerun.txt",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         features = "src/test/resources/features",
